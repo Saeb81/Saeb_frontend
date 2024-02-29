0907b31308dc7ddd9543b71053adf2f66a561a4a
Saeb81 <saebsobhi9843@gmail.com>
2024-03-01 01:36:14 +0330

    8 commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -303,8 +303,6 @@ export default function PrimarySearchAppBar() {
 
             </Container>
 
-
-
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ display: { xs: 'none', md: 'flex', } }}>
               <IconButton size="large" aria-label="show 4 new mails" color="inherit">
