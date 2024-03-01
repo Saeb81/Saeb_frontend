d39445b50fc713874afe1d42619c106c2db7f81b
Saeb81 <saebsobhi9843@gmail.com>
2024-03-01 12:43:31 +0330

    16 commit

--- src/Log/Login/Login.jsx
+++ src/Log/Login/Login.jsx
@@ -8,44 +8,27 @@ import Input from '@mui/material/Input';
 import Button from '@mui/material/Button';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
-
 import Box from '@mui/material/Box';
 import AccountCircle from '@mui/icons-material/AccountCircle';
-
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
-
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
-
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
-
-
 import { Link } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import { get } from '../../utils/httpClient'
-
 import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
-    const [user, setUser] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const [loggedInUser, setLoggedInUser] = useState(null);
-
     const [showPassword, setShowPassword] = React.useState(false);
-
     const loadUser = async () => {
         const data = await get('/users')
-        
         setUser(data);
     }
     useEffect(() => {
@@ -102,17 +85,13 @@ export default function Login() {
     const handleLogin = async () => {
 
         const data = await get('/users')
-        console.log(data);
         let i = 0;
         while (i < data.length) {
 
            
             if (data[i].username == username && data[i].password == password) {
-
-                
                 localStorage.setItem('user_id', data[i].user_id);
                 const storedUserId = localStorage.getItem('user_id');
-                console.log(storedUserId)
                 navigate('/Home')
             }
             i++;
